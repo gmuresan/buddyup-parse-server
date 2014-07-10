223e829e29f9ed4df79c66f2855f1f895991142d
George Muresan <gmuresan3@gmail.com>
2014-07-10 15:32:50 -0400

    added getNewData for chats

--- cloud/main.js
+++ cloud/main.js
@@ -6,6 +6,25 @@ Parse.Cloud.define("hello", function(request, response) {
 });
 
 Parse.Cloud.afterSave("FriendRelation", function(request) {
+
+  updateMutualFriends = false;
+
+  previousStatus = request.object.previous("status");
+  if(previousStatus == "friends" || previousStatus == "unfriended") {
+    currentStatus = request.object.get("status");
+
+    if(currentStatus == "friends" || currentStatus == "unfriended") {
+      updateMutualFriends = true;
+    }
+  }
+
+  if(!updateMutualFriends) {
+    //response.success();
+    return;
+  }
+
+  request.object.save({silent: true});
+
   user = request.object.get("fromUser");
   friend = request.object.get("toUser");
 
@@ -28,16 +47,77 @@ Parse.Cloud.afterSave("FriendRelation", function(request) {
             mutualFriendsRelation.add(friendRelation.get("toUser"))
           }
 
-          request.object.save({silent:true});
+          //response.success();
+        },
+
+        error: function(error) {
+          console.error("Got an error " + error.code + " : " + error.message);
+          //response.error();
         }
+
       })
       
     },
 
     error: function(error) {
       console.error("Got an error " + error.code + " : " + error.message);
+      //response.error();
     }
   });
 
 });
+
+
+Parse.Cloud.define("getNewData", function(request, response) {
+  
+  var sinceDate = request.since;
+  var newSince = new Date();
+
+  var chatsQuery = new Parse.Query("Chat");
+  chatsQuery.equalTo("members", request.user);
+
+  var messageQuery = new Parse.Query("ChatMessage");
+  messageQuery.matchesQuery("chat", chatsQuery).addAscending("createdAt").include("chat");
+  if(sinceDate) {
+    messageQuery.greaterThanOrEqualTo("createdAt", sinceDate);
+  }
+  messageQuery.find({
+    success: function(messages) {
+      console.log(messages);
+      chats = {}
+      for(i=0; i<messages.length; i++) {
+        chat = messages[i].get("chat");
+        chats[chat.id] = chat;
+      }
+
+      chatList = []
+      for(var key in chats) {
+        chatList.push(key);
+      }
+
+      var chatQuery = new Parse.Query("Chat");
+      chatQuery.containedIn("objectId", chatList).include("members");
+
+      chatQuery.find({
+        success: function(chatList) {
+          response.success({"messages": messages, "chats": chatList});
+        },
+
+        error: function(error) {
+          response.error(error);
+        }
+      });
+
+    },
+
+    error: function(error) {
+      response.error(error);
+    }
+  })
+
+});
+
+
+
+
 
